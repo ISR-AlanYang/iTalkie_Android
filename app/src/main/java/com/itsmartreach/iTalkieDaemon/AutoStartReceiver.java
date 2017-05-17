5a19cdd08435884d9683dd244899ba5cd311e9f5
Alan Yang <alan.yang@itsmartreach.com>
2017-05-17 11:19:12 +0800

    add auto start receiver

--- app/src/main/java/com/itsmartreach/iTalkieDaemon/AutoStartReceiver.java
+++ app/src/main/java/com/itsmartreach/iTalkieDaemon/AutoStartReceiver.java
@@ -5,6 +5,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.itsmartreach.libzm.Constants;
+
+import static android.R.attr.autoStart;
 
 public class AutoStartReceiver extends BroadcastReceiver {
     public AutoStartReceiver() {
@@ -16,7 +21,10 @@ public class AutoStartReceiver extends BroadcastReceiver {
         // an Intent broadcast.
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
 
+
         if ( preferences != null && preferences.getBoolean("auto_start",true) ){
+            Log.i(Constants.TAG,"autoStart = true");
+
             context.startActivity(new Intent(context, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
         }
     }
